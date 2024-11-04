208503648e3ad0b6ecca0d77ab309847ad7094a4
mgulates <mervegul.ates@ogr.sakarya.edu.tr>
2024-11-04 14:43:45 +0300

    mert

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+358ab0f69af5089e1c854d66b676b89a618bc5d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf57b5cdfc6bb1fd6c0f5094ae0514807c5491d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
